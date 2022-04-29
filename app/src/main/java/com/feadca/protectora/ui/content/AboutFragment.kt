e38a6b6e819a09b303d6f31d2cc61bb56bba4135
fadria <fadriacarrasco@gmail.com>
2022-04-29 21:26:59 +0200

    Reorganización de carpetas

--- app/src/main/java/com/feadca/protectora/ui/content/AboutFragment.kt
+++ app/src/main/java/com/feadca/protectora/ui/content/AboutFragment.kt
@@ -1,10 +1,11 @@
-package com.feadca.protectora
+package com.feadca.protectora.ui.content
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.feadca.protectora.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GraphicsFragment.newInstance] factory method to
+ * Use the [AboutFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GraphicsFragment : Fragment() {
+class AboutFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class GraphicsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graphics, container, false)
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class GraphicsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment GraphicsFragment.
+         * @return A new instance of fragment AboutFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GraphicsFragment().apply {
+            AboutFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/feadca/protectora/ui/content/AnimalsFragment.kt
+++ app/src/main/java/com/feadca/protectora/ui/content/AnimalsFragment.kt
@@ -1,10 +1,11 @@
-package com.feadca.protectora
+package com.feadca.protectora.ui.content
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.feadca.protectora.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DonationsFragment.newInstance] factory method to
+ * Use the [AnimalsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DonationsFragment : Fragment() {
+class AnimalsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class DonationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donations, container, false)
+        return inflater.inflate(R.layout.fragment_animals, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class DonationsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DonationsFragment.
+         * @return A new instance of fragment AnimalsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DonationsFragment().apply {
+            AnimalsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
