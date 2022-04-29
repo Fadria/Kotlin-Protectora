ade2e4cdce3939c20ef935c84c9e5272148deaf0
fadria <fadriacarrasco@gmail.com>
2022-04-30 01:19:55 +0200

    WIP: fragmento about

--- app/src/main/java/com/feadca/protectora/ui/content/AboutFragment.kt
+++ app/src/main/java/com/feadca/protectora/ui/content/AboutFragment.kt
@@ -4,58 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.feadca.protectora.R
-import com.feadca.protectora.ui.MainActivity
+import com.feadca.protectora.databinding.FragmentAboutBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AboutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class AboutFragment : Fragment(R.layout.fragment_about) {
+    // Enlace con las vistas
+    private var fragmentAboutBinding: FragmentAboutBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        // Inflamos el layout del fragmento about
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AboutFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Actualizamos la vinculación a la vista
+        val binding = FragmentAboutBinding.bind(view)
+        fragmentAboutBinding = binding
+
+        // Cuando pulsemos el botón ver animales cambiaremos el fragmento
+        binding.btnWatchAnimals.setOnClickListener {
+            Toast.makeText(requireContext(), "hola", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Operaciones realizadas al finalizar la vista
+    override fun onDestroyView() {
+        fragmentAboutBinding = null
+        super.onDestroyView()
     }
 }
