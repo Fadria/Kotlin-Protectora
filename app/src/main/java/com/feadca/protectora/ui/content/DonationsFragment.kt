1a55b05260d8dd3a4da4a8e41726c443286339d2
fadria <fadriacarrasco@gmail.com>
2022-05-01 19:04:10 +0200

    WIP: Pago con PayPal

--- app/src/main/java/com/feadca/protectora/ui/content/DonationsFragment.kt
+++ app/src/main/java/com/feadca/protectora/ui/content/DonationsFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.feadca.protectora.R
 import com.feadca.protectora.databinding.FragmentDonationsBinding
 import com.feadca.protectora.ui.MainActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 class DonationsFragment : Fragment() {
     private var fragmentDonationsBinding: FragmentDonationsBinding? = null
@@ -28,21 +30,44 @@ class DonationsFragment : Fragment() {
         binding.btnBizum.setOnClickListener {
             MaterialAlertDialogBuilder(requireContext())
                 .setTitle(getString(R.string.donation_bizum))
-                .setMessage("Puede donar enviando un Bizum al teléfono 666 999 333 con su donación.\n\n" +
-                        "Recuerde que cada cuenco de comida cuesta 1€. Muchas gracias.")
+                .setMessage(
+                    "Puede donar enviando un Bizum al teléfono 666 999 333 con su donación.\n\n" +
+                            "Recuerde que cada cuenco de comida cuesta 1€. Muchas gracias."
+                )
                 .setNeutralButton(getString(R.string.cerrar)) { _, _ -> }
                 .show()
         }
 
         binding.btnTransfer.setOnClickListener {
             MaterialAlertDialogBuilder(requireContext())
-                .setTitle(getString(R.string.donation_bizum))
-                .setMessage("Puede donar enviando una transferencia al siguiente IBAN con su donación.\n\n" +
-                        "IBAN: ES6500493732237968060296\n\n" +
-                        "Recuerde que cada cuenco de comida cuesta 1€. Muchas gracias.")
+                .setTitle(getString(R.string.donation_bank))
+                .setMessage(
+                    "Puede donar enviando una transferencia al siguiente IBAN con su donación.\n\n" +
+                            "IBAN: ES6500493732237968060296\n\n" +
+                            "Recuerde que cada cuenco de comida cuesta 1€. Muchas gracias."
+                )
                 .setNeutralButton(getString(R.string.cerrar)) { _, _ -> }
                 .show()
         }
 
+        binding.btnPayPal.setOnClickListener {
+            var donationAmountText = binding.etDonation.text.toString()
+            if (donationAmountText != "") {
+                val donationAmount = donationAmountText.toDouble()
+                if (donationAmount > 0) {
+                    showSnackbar("WIP: PayPal")
+                } else {
+                    showSnackbar("Por favor, introduzca una cantidad superior a 0")
+                }
+            }else{
+                showSnackbar("Por favor, introduzca la cantidad a donar")
+            }
+        }
+    }
+
+    // Función encargada de mostrar avisos
+    private fun showSnackbar(message: String) {
+        Snackbar.make(fragmentDonationsBinding!!.layout, message, Snackbar.LENGTH_SHORT)
+            .show()
     }
 }
